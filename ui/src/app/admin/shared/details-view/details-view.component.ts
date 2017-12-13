dafeb3474adec8e2e560f887038a57557b3d0031
Andzej Maciusovic <andzej.maciusovic@gmail.com>
2017-12-13 21:00:20 +0200

    add loading overlay

--- ui/src/app/admin/shared/details-view/details-view.component.ts
+++ ui/src/app/admin/shared/details-view/details-view.component.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { routerDetailsTransition } from '../../animations/route-details';
+import { trigger, state, animate, style, transition } from '@angular/core';
+
+export function slideToLeftTransition() {
+    return slideToLeft();
+}
+
+function slideToLeft() {
+    return trigger('slideToLeftTransition', [
+        state('void', style({ position: 'absolute', width: '80%' })),
+        state('*', style({ position: 'absolute', width: '80%' })),
+        transition(':enter', [  // before 2.1: transition('void => *', [
+            style({ transform: 'translateX(100%)' }),
+            animate('0.2s ease-in-out', style({ transform: 'translateX(0%)' }))
+        ]),
+        transition(':leave', [  // before 2.1: transition('* => void', [
+            style({ transform: 'translateX(0%)' }),
+            animate('0.2s ease-in-out', style({ transform: 'translateX(100%)' }))
+        ])
+    ]);
+}
+
 
 @Component({
     selector: 'app-details-view',
     templateUrl: './details-view.component.html',
     styleUrls: ['./details-view.component.scss'],
-    animations: [routerDetailsTransition()],
+    animations: [slideToLeftTransition()],
     // tslint:disable-next-line:use-host-property-decorator
-    host: { '[@routerDetailsTransition]': '' }
+    host: { '[@slideToLeftTransition]': '' }
 })
 export class DetailsViewComponent implements OnInit {
 
