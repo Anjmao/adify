ba79b2a3b664d521c4b225028d03a5e7e2232947
Andzej Maciusovic <andzej.maciusovic@gmail.com>
2018-01-07 17:51:52 +0200

    auth service

---
+++ auth/app-standalone.go
@@ -0,0 +1,19 @@
+// +build !appengine,!appenginevm
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func createMux() *echo.Echo {
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+	return e
+}
+
+func main() {
+	e.Logger.Fatal(e.Start(":8081"))
+}
